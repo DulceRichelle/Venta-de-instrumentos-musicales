9be85d59adf587cd7fa96dab8c953818e55fec60
Carlos Cervera <carlos.cerveratorres@cieep.com>
2025-04-06 04:51:04 +0200

    add components, navbar, config routes

--- angular/proyectoLM/src/app/app.routes.ts
+++ angular/proyectoLM/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { PageNotFoundComponent } from './componentes/page-not-found/page-not-found.component';
+import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { TiendaComponent } from './componentes/tienda/tienda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'tienda', component: TiendaComponent},
+
+  {path: '**', component: PageNotFoundComponent}
+];
